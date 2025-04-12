7c41d963fd68c0c68bb572ef0b8f44996335be30
Ivan Pronin <ivo-pronin@yandex.com>
2025-04-12 14:31:09 +0300

    Initial commit

---
+++ src/main/java/edu/bit/kuber_demo/KuberDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.bit.kuber_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuberDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bit/kuber_demo/KuberDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bit.kuber_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
