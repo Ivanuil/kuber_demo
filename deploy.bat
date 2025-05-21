7eff917ac3ce16ed8197523286f8aa03df4bf54a
Ivan Pronin <ivo-pronin@yandex.com>
2025-05-21 19:37:22 +0300

    hw3

--- deploy.bat
+++ deploy.bat
@@ -9,3 +9,6 @@ call kubectl apply -f .\k8s\gateway.yaml
 call kubectl apply -f .\k8s\peer-authentication.yaml
 call kubectl apply -f .\k8s\virtual-service.yaml
 call kubectl apply -f .\k8s\destination-rules.yaml
+
+call kubectl apply -f prometheus.yaml
+call kubectl apply -f service-monitor.yaml
