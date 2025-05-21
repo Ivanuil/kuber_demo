b921a90db05aeb64e864b1ffe1d6c0f6834fde75
Ivan Pronin <ivo-pronin@yandex.com>
2025-05-21 16:44:07 +0300

    Дополнил deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -4,3 +4,8 @@ call kubectl apply -f .\k8s\deployment.yaml
 call kubectl apply -f .\k8s\service.yaml
 call kubectl apply -f .\k8s\daemon-set.yaml
 call kubectl apply -f .\k8s\cron-job.yaml
+
+call kubectl apply -f .\k8s\gateway.yaml
+call kubectl apply -f .\k8s\peer-authentication.yaml
+call kubectl apply -f .\k8s\virtual-service.yaml
+call kubectl apply -f .\k8s\destination-rules.yaml
